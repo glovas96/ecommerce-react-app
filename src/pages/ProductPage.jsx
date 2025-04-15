8fde562d4232d0fce6c330c34b43c91e6fac0492
glovas96 <alekceygolovko@gmail.com>
2025-04-15 11:03:24 +0300

    feat: integrate addToCart action into ProductPage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../features/cart/cartSlice";
 
 const ProductPage = () => {
     // Get product ID from URL
@@ -8,12 +10,27 @@ const ProductPage = () => {
     // Local state for a single product
     const [product, setProduct] = useState(null);
 
+    // Redux dispatch function
+    const dispatch = useDispatch();
+
     useEffect(() => {
         // Fetch product by ID
         fetch(`https://dummyjson.com/products/${id}`)
             .then((res) => res.json())
             .then(setProduct);
     }, [id]);
+
+    // Add product to cart
+    const handleAdd = () => {
+        dispatch(
+            addToCart({
+                id: product.id,
+                title: product.title,
+                price: product.price,
+                thumbnail: product.thumbnail,
+            })
+        );
+    };
 
     // Show loading state
     if (!product) return <div>Loading...</div>;
@@ -23,6 +40,9 @@ const ProductPage = () => {
             <h1>{product.title}</h1>
             <p>Price: {product.price} $</p>
             <img src={product.thumbnail} width={200} />
+
+            {/* Add to cart button */}
+            <button onClick={handleAdd}>Add to cart</button>
         </div>
     );
 };
